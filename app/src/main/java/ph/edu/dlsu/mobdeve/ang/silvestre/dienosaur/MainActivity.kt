5443d9c7de52cd619dca8803bc8c6bba36fea8f7
czeska120 <franczeska_silvestre@dlsu.edu.ph>
2023-03-12 14:16:05 +0800

    Fixed TextOutline

--- app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/MainActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/MainActivity.kt
@@ -2,6 +2,8 @@ package ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // Hides title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         setContentView(binding.root)
     }
 }
