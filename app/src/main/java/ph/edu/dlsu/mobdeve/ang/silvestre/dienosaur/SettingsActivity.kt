26dae1976616fc67e818ceabc1c278f40c1b300c
czeska120 <franczeska_silvestre@dlsu.edu.ph>
2023-03-13 23:57:04 +0800

    Created slider with Toast

--- app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/SettingsActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/SettingsActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import android.widget.SeekBar
+import android.widget.Toast
 import ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
@@ -17,5 +19,36 @@ class SettingsActivity : AppCompatActivity() {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(binding.root)
+
+        binding.seekbarSoundfx.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // intentionally empty; required to override
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // intentionally empty; required to override
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                val progress = seekBar.progress
+                Toast.makeText(applicationContext, "Sound FX volume: $progress", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        binding.seekbarMusic.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // intentionally empty; required to override
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // intentionally empty; required to override
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                val progress = seekBar.progress
+                Toast.makeText(applicationContext, "Music volume: $progress", Toast.LENGTH_SHORT).show()
+            }
+        })
+
     }
 }
