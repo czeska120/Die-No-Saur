130c8dbbea34b09c6b04e3e2a7d223542597adf1
czeska120 <franczeska_silvestre@dlsu.edu.ph>
2023-03-14 16:10:09 +0800

    Created Help fragment

--- app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/MainActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/MainActivity.kt
@@ -3,6 +3,7 @@ package ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.view.animation.AlphaAnimation
@@ -18,6 +19,9 @@ class MainActivity : AppCompatActivity() {
         // Hides title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        //Hides action bar (bottom)
+         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
         setContentView(binding.root)
 
--- app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/SettingsActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/SettingsActivity.kt
@@ -3,11 +3,15 @@ package ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.SeekBar
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction.TRANSIT_FRAGMENT_FADE
 import ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur.databinding.ActivitySettingsBinding
+import ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur.fragments.FragmentHelp1
 
 class SettingsActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingsBinding
@@ -16,9 +20,19 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
 
+        // Hides title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        //Hides action bar (bottom)
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
         setContentView(binding.root)
+
+        binding.settingsHelp.setOnClickListener{
+            loadFragment(FragmentHelp1())
+
+        }
 
         binding.seekbarSoundfx.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
@@ -49,6 +63,22 @@ class SettingsActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "Music volume: $progress", Toast.LENGTH_SHORT).show()
             }
         })
+
+    }
+
+
+
+    private fun loadFragment(fragment: Fragment) {
+        // create a FragmentManager
+        val fm = supportFragmentManager
+
+        // create a FragmentTransaction to begin the transaction and replace the Fragment
+        val fragmentTransaction = fm.beginTransaction()
+
+        // replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(R.id.settings_framelayout, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit() // save the changes
 
     }
 }
