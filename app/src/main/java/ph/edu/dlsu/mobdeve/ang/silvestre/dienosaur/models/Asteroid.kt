ba0ef138c0c56edbb9bafa703c9d504702cd6098
elliamaeang <elliamae_ang@dlsu.edu.ph>
2023-03-25 18:02:38 +0800

    Change asteroid from GIF to sprite sheet

--- app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/models/Asteroid.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/models/Asteroid.kt
@@ -7,24 +7,31 @@ import ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur.R
 import kotlin.random.Random
 
 class Asteroid(context: Context) {
-    private var asteroid: Bitmap
+    private var asteroid: Array<Bitmap>
+    var asteroidFrame: Int = 0
     var asteroidX: Int = 0
     var asteroidY: Int = 0
     var velocity: Int = 0
     private val random: Random = Random.Default
 
     init {
-        asteroid = BitmapFactory.decodeResource(context.resources, R.drawable.asteroid_large)
+        asteroid = arrayOf(
+            BitmapFactory.decodeResource(context.resources, R.drawable.a_small_1),
+            BitmapFactory.decodeResource(context.resources, R.drawable.a_small_2),
+            BitmapFactory.decodeResource(context.resources, R.drawable.a_small_3),
+            BitmapFactory.decodeResource(context.resources, R.drawable.a_small_4),
+            BitmapFactory.decodeResource(context.resources, R.drawable.a_small_5),
+        )
         resetPosition()
     }
 
-    fun getAsteroid(): Bitmap {
-        return asteroid
+    fun getAsteroid(frame: Int): Bitmap {
+        return asteroid[frame]
     }
 
     fun resetPosition() {
-        asteroidX = random.nextInt(GameView.dWidth - asteroid.width)
-        asteroidY = -200 + random.nextInt(600) * -1
-        velocity = 35 + random.nextInt(16)
+        asteroidX = random.nextInt(GameView.dWidth - asteroid[0].width)
+        asteroidY = -100 + random.nextInt(600) * -1
+        velocity = 35 + random.nextInt(10)
     }
 }
