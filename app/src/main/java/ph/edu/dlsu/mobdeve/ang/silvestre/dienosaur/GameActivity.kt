dd6b891dbe062243d74ed3c2946468738c0f9309
elliamaeang <elliamae_ang@dlsu.edu.ph>
2023-03-28 17:52:34 +0800

    Comment out code used for testing

--- app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/GameActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/GameActivity.kt
@@ -2,11 +2,15 @@ package ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur.databinding.ActivityGameBinding
 import ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur.models.GameView
 
 class GameActivity : AppCompatActivity() {
+    //private lateinit var binding: ActivityGameBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(GameView(this))
     }
 }
