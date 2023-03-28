ba5a65002349ced2a2362c55cf060ba3632cccaa
elliamaeang <elliamae_ang@dlsu.edu.ph>
2023-03-28 21:03:40 +0800

    Update sprite model classes

--- app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/models/DinoSprite.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/models/DinoSprite.kt
@@ -5,18 +5,35 @@ import ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur.R
 class DinoSprite(
     val name: String,
     val walk: Int,
-    val run: Int,
-    val hit: Int,
+    val run: Array<Int>,
+    val hit: Array<Int>,
     val dead: Int
 ) {
-
+    var runFrame: Int = 0
+    var hitFrame: Int = 0
 }
 
-var Dinos = arrayListOf<DinoSprite>(
-    DinoSprite("Nico", R.drawable.nico, R.drawable.nico_run, R.drawable.nico_hit, R.drawable.nico_run),
-    DinoSprite("Doux", R.drawable.doux, R.drawable.doux_run, R.drawable.doux_hit, R.drawable.doux_dead),
-    DinoSprite("Kira", R.drawable.kira, R.drawable.kira_run, R.drawable.kira_hit, R.drawable.kira_dead),
-    DinoSprite("Kuro", R.drawable.kuro, R.drawable.kuro_run, R.drawable.kuro_hit, R.drawable.kuro_dead),
-    DinoSprite("Mono", R.drawable.mono, R.drawable.mono_run, R.drawable.mono_hit, R.drawable.mono_dead),
-    DinoSprite("Olaf", R.drawable.olaf, R.drawable.olaf_run, R.drawable.olaf_hit, R.drawable.olaf_dead)
+// run sprites
+private val nico_run = arrayOf<Int>(R.drawable.nico_run_1, R.drawable.nico_run_2, R.drawable.nico_run_3, R.drawable.nico_run_4, R.drawable.nico_run_5, R.drawable.nico_run_6)
+private val doux_run = arrayOf<Int>(R.drawable.doux_run_1, R.drawable.doux_run_2, R.drawable.doux_run_3, R.drawable.doux_run_4, R.drawable.doux_run_5, R.drawable.doux_run_6)
+private val kira_run = arrayOf<Int>(R.drawable.kira_run_1, R.drawable.kira_run_2, R.drawable.kira_run_3, R.drawable.kira_run_4, R.drawable.kira_run_5, R.drawable.kira_run_6)
+private val kuro_run = arrayOf<Int>(R.drawable.kuro_run_1, R.drawable.kuro_run_2, R.drawable.kuro_run_3, R.drawable.kuro_run_4, R.drawable.kuro_run_5, R.drawable.kuro_run_6)
+private val mono_run = arrayOf<Int>(R.drawable.mono_run_1, R.drawable.mono_run_2, R.drawable.mono_run_3, R.drawable.mono_run_4, R.drawable.mono_run_5, R.drawable.mono_run_6)
+private val olaf_run = arrayOf<Int>(R.drawable.olaf_run_1, R.drawable.olaf_run_2, R.drawable.olaf_run_3, R.drawable.olaf_run_4, R.drawable.olaf_run_5, R.drawable.olaf_run_6)
+
+// hit sprites
+private val nico_hit = arrayOf<Int>(R.drawable.nico_hit_1, R.drawable.nico_hit_2, R.drawable.nico_hit_3, R.drawable.nico_hit_4)
+private val doux_hit = arrayOf<Int>(R.drawable.doux_hit_1, R.drawable.doux_hit_2, R.drawable.doux_hit_3, R.drawable.doux_hit_4)
+private val kira_hit = arrayOf<Int>(R.drawable.kira_hit_1, R.drawable.kira_hit_2, R.drawable.kira_hit_3, R.drawable.kira_hit_4)
+private val kuro_hit = arrayOf<Int>(R.drawable.kuro_hit_1, R.drawable.kuro_hit_2, R.drawable.kuro_hit_3, R.drawable.kuro_hit_4)
+private val mono_hit = arrayOf<Int>(R.drawable.mono_hit_1, R.drawable.mono_hit_2, R.drawable.mono_hit_3, R.drawable.mono_hit_4)
+private val olaf_hit = arrayOf<Int>(R.drawable.olaf_hit_1, R.drawable.olaf_hit_2, R.drawable.olaf_hit_3, R.drawable.olaf_hit_4)
+
+val Dinos = arrayListOf<DinoSprite>(
+    DinoSprite("Nico", R.drawable.nico, nico_run, nico_hit, R.drawable.nico_run),
+    DinoSprite("Doux", R.drawable.doux, doux_run, doux_hit, R.drawable.doux_dead),
+    DinoSprite("Kira", R.drawable.kira, kira_run, kira_hit, R.drawable.kira_dead),
+    DinoSprite("Kuro", R.drawable.kuro, kuro_run, kuro_hit, R.drawable.kuro_dead),
+    DinoSprite("Mono", R.drawable.mono, mono_run, mono_hit, R.drawable.mono_dead),
+    DinoSprite("Olaf", R.drawable.olaf, olaf_run, olaf_hit, R.drawable.olaf_dead)
 )
--- app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/models/Explosion.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/models/Explosion.kt
@@ -6,11 +6,19 @@ import android.graphics.BitmapFactory
 import ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur.R
 
 class Explosion(private val context: Context) {
-    private val explosion: Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.explosion_large)
+    private val explosion: Array<Bitmap> = arrayOf(
+        BitmapFactory.decodeResource(context.resources, R.drawable.e_small_1),
+        BitmapFactory.decodeResource(context.resources, R.drawable.e_small_2),
+        BitmapFactory.decodeResource(context.resources, R.drawable.e_small_3),
+        BitmapFactory.decodeResource(context.resources, R.drawable.e_small_4),
+        BitmapFactory.decodeResource(context.resources, R.drawable.e_small_5),
+        BitmapFactory.decodeResource(context.resources, R.drawable.e_small_6)
+    )
+    var explosionFrame: Int = 0
     var explosionX: Int = 0
     var explosionY: Int = 0
 
-    fun getExplosion(): Bitmap {
-        return explosion
+    fun getExplosion(frame: Int): Bitmap {
+        return explosion[frame]
     }
 }
