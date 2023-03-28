e26b7161ec728aef4d669160297e7669cea930ac
czeska120 <franczeska_silvestre@dlsu.edu.ph>
2023-03-28 19:47:34 +0800

    Insert data into db

--- app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/GameOverActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/GameOverActivity.kt
@@ -3,16 +3,25 @@ package ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur.databinding.ActivityGameOverBinding
 import ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur.fragments.FragmentBottomBtns
 
 class GameOverActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGameOverBinding
+    private lateinit var user: FirebaseUser
+    private lateinit var dbreference: DatabaseReference
+    private lateinit var mAuth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameOverBinding.inflate(layoutInflater)
+        mAuth = FirebaseAuth.getInstance()
         setContentView(binding.root)
 
         val frame2 = R.id.settings_framelayout2
@@ -21,6 +30,23 @@ class GameOverActivity : AppCompatActivity() {
         val data = intent.extras
         val score = data?.getString("score")
         binding.playerScore.text = score
+
+        val currentUser = mAuth.currentUser
+        dbreference = FirebaseDatabase.getInstance().reference
+
+//        if(user != null){
+        if (currentUser != null) {
+            dbreference.child("Score").child(currentUser.uid).child("result").setValue(score)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Score added", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this, "Something went wrong.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+//        }
+
 
         binding.btnPlayAgain.setOnClickListener{
             val startGame = Intent(this, GameActivity::class.java)
