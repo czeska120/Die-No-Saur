5a6d75ea9e009c465eec9598b5d62d87ecbcc657
elliamaeang <elliamae_ang@dlsu.edu.ph>
2023-03-29 22:54:44 +0800

    Use activity_game.xml

--- app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/GameActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/GameActivity.kt
@@ -2,13 +2,15 @@ package ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
-    //private lateinit var binding: ActivityGameBinding
+    private lateinit var binding: ActivityGameBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //binding = ActivityGameBinding.inflate(layoutInflater)
-        setContentView(GameView(this))
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(GameView(this))
     }
 }
