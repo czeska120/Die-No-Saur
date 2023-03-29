672e91ae6edad5e0a38a2fc6126517966445316e
elliamaeang <elliamae_ang@dlsu.edu.ph>
2023-03-30 00:01:03 +0800

    Pause button functionality

--- app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/GameActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/ang/silvestre/dienosaur/GameActivity.kt
@@ -1,8 +1,12 @@
 package ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.alpha
+import androidx.fragment.app.Fragment
 import ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur.databinding.ActivityGameBinding
+import ph.edu.dlsu.mobdeve.ang.silvestre.dienosaur.fragments.FragmentGamePause
 
 class GameActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGameBinding
@@ -12,5 +16,25 @@ class GameActivity : AppCompatActivity() {
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //setContentView(GameView(this))
+
+        val frame = binding.gameFramelayout.id
+
+        binding.btnPause.setOnClickListener{
+            binding.btnPause.visibility = View.INVISIBLE //hide pause button
+            loadFragment(frame, FragmentGamePause()) //show pause screen
+        }
+    }
+
+    private fun loadFragment(frame:Int, fragment: Fragment) {
+        // create a FragmentManager
+        val fm = supportFragmentManager
+
+        // create a FragmentTransaction to begin the transaction and replace the Fragment
+        val fragmentTransaction = fm.beginTransaction()
+
+        // replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(frame, fragment)
+        fragmentTransaction.commit() // save the changes
+
     }
 }
